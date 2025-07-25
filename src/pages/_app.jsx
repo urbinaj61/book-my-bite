1e7c1efd6db6c506433b22481211cc9c2f66583b
Joseph <joseph.urbina61@hotmail.com>
2025-07-25 11:05:20 +0200

    added homepage and retrived restaurants

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,12 @@
+import { SWRConfig } from "swr";
 import "@/styles/globals.css";
 
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
