58d958141de7125c3885599fd635e032bbe29475
Joseph <joseph.urbina61@hotmail.com>
2025-07-28 14:27:51 +0200

    added more bookings data and created content components

--- src/pages/bookings/index.jsx
+++ src/pages/bookings/index.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import useSWR from "swr";
 import ShowMyBookings from "@/components/showMyBookings/ShowMyBookings";
-import { useState } from "react";
+import ShowMyBookingsContent from "@/components/showMyBookingsContent/ShowMyBookingsContent";
 
 const Bookings = () => {
   const [email, setEmail] = useState("");
@@ -10,71 +11,17 @@ const Bookings = () => {
 
   const { data, error, isLoading } = useSWR(swrPath);
 
-  let content = null;
-
-  if (!email) {
-    content = (
-      <>
-        <p>Please enter your email to view your bookings.</p>
-      </>
-    );
-  } else if (isLoading) {
-    content = (
-      <>
-        <p>Loading bookings for {email}...</p>
-      </>
-    );
-  } else if (error) {
-    content = (
-      <>
-        <p>Error loading bookings for {email}. Please try again.</p>
-      </>
-    );
-  } else if (data) {
-    if (data.length === 0) {
-      content = (
-        <>
-          <p>No bookings found for {email}.</p>
-        </>
-      );
-    } else {
-      content = (
-        <>
-          <h2>Your Bookings:</h2>
-          <ul>
-            {data.map((booking) => (
-              <li key={booking._id}>
-                <strong>Restaurant:</strong> {booking.restaurantId?.name}
-                <br />
-                <strong>Date:</strong> {booking.dateBooked}
-                <br />
-                <strong>Time:</strong> {booking.timeSlot?.start} -{" "}
-                {booking.timeSlot?.end}
-                <br />
-                <strong>Table:</strong> {booking.tableBooked}
-                <br />
-                <strong>Seats:</strong> {booking.seatsBooked}
-                <hr />
-              </li>
-            ))}
-          </ul>
-        </>
-      );
-    }
-  } else {
-    content = (
-      <>
-        <p>An unexpected error occurred or data format is incorrect.</p>
-      </>
-    );
-  }
-
   return (
     <>
       <div>
         <h1>Bookings page</h1>
         <ShowMyBookings setEmail={setEmail} />
-        {content}
+        <ShowMyBookingsContent
+          email={email}
+          data={data}
+          error={error}
+          isLoading={isLoading}
+        />
       </div>
     </>
   );
